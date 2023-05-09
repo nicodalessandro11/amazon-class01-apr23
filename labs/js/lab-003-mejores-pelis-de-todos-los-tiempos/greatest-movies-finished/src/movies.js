db608c21f3a73fdeebf9caac9f3e2ea6aa4d03f6
Diego Zito <diegozito.m@gmail.com>
2023-05-09 17:05:54 +0200

    upload

--- labs/js/lab-003-mejores-pelis-de-todos-los-tiempos/greatest-movies-finished/src/movies.js
+++ labs/js/lab-003-mejores-pelis-de-todos-los-tiempos/greatest-movies-finished/src/movies.js
@@ -8,24 +8,74 @@ const getAllDirectors = (movies) => {
   let directoresFiltrados = directoresSinFiltrar.filter((director, index) => {
     return directoresSinFiltrar.indexOf(director) === index;
   });
-  console.log(directoresFiltrados);
+  //console.log(directoresFiltrados);
   return directoresFiltrados;
 };
-getAllDirectors(movies);
+//getAllDirectors(movies);
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+const howManyMovies = (movies) => {
+  let stevenDramaPelis = movies.filter((pelicula) => {
+    return (
+      pelicula.director === "Steven Spielberg" &&
+      pelicula.genre.includes("Drama")
+    );
+  });
+  // console.table(stevenDramaPelis);
+  return stevenDramaPelis;
+};
+//howManyMovies(movies);
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+const scoresAverage = (movies) => {
+  if (movies.length === 0) return 0;
+  // usando el reduce apuntamos al movies y le pasamos el metodo con 2 argumentos
+  let puntuacion = movies.reduce((acumulador, valorActual) => {
+    if (valorActual.score) {
+      return acumulador + valorActual.score;
+    } else {
+      return acumulador;
+    }
+  }, 0);
+  let average = (puntuacion / movies.length).toFixed(2);
+  //console.log(`${Number(average)} - test`);
+  return Number(average);
+};
+scoresAverage(movies);
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+const dramaMoviesScore = (movies) => {
+  if (movies.length === 0) return 0;
+  let dramaMovies = movies.filter((pelicula) =>
+    pelicula.genre.includes("Drama")
+  );
+  // console.log(scoresAverage(dramaMovies));
+  return scoresAverage(dramaMovies);
+};
+// dramaMoviesScore(movies);
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+const orderByYear = (movies) => {
+  // part 1 - mappear pelis
+  let peliculas = movies.map((pelicula) => pelicula);
+  // part 2  - sortear con sort()
+  peliculas.sort((peliA, peliB) => {
+    if (peliA.year > peliB.year) return peliA.year - peliB.year;
+    if (peliA.year < peliB.year) return peliA.year - peliB.year;
+    else return peliA.title.localeCompare(peliB.title);
+  });
+  console.log(peliculas);
+  return peliculas;
+};
+orderByYear(movies);
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+const orderAlphabetically = (movies) => {
+  return movies
+    .map((pelicula) => pelicula.title)
+    .sort()
+    .slice(0, 20);
+};
+console.table(orderAlphabetically(movies));
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
